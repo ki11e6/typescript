d4438e8e0efd5dbe4d725086e5d1d028d60cc3b5
ki11e6 <sharath.surendran.m@gmail.com>
2023-07-31 17:09:29 +0530

    advanced types init

--- base-1/dist/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    console.log('button clicked!');
-});
--- base-1/src/script.ts
+++
@@ -1,4 +0,0 @@
-const btn = document.querySelector('button')!;
-btn.addEventListener('click', () => {
-  console.log('button clicked!');
-});
